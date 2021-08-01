da1995ed8616884b81287cb5e1d4915688055a1d
Bunks <busk_1@yahoo.com>
2021-07-31 21:38:26 -0400

    added argv to start up of server

--- client.js
+++ client.js
@@ -1,7 +1,12 @@
 const io = require("socket.io-client");
 
-const socket = io();
+const serverIp = process.argv[2];
+const port = process.argv[3];
+
+const socket = io(`http://${serverIp}:${port}`);
 
 socket.on("connect", () => {
     console.log("Client has connected with id: " + socket.id);
 })
+
+console.log(process.argv);
