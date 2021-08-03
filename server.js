cde0da3634e55077f9b0028164374928a74c99cc
Bunks <busk_1@yahoo.com>
2021-08-03 16:37:39 -0400

    added convert socket to player function

--- server.js
+++ server.js
@@ -5,43 +5,58 @@ const PORT = 5050;
 
 let usersConnected = 0;
 const clientsConnected = {};
-const players = {};
-let unmatched;
+const firstPlayer = {};
+const secondPlayer = {};
 
-const addClient = socket => {
-    console.log("Client has connected.", socket.id);
-    clientsConnected[socket.id] = socket;
-    usersConnected++;
-    if (usersConnected === 2 ) {
-        console.log("Starting the game.")
-        socket.broadcast.emit("game-start")
-    }
+const addClient = (socket) => {
+  console.log("Client has connected.", socket.id);
+  clientsConnected[socket.id] = socket;
+  convertSocketToPlayer(socket);
+  usersConnected++;
+  if (usersConnected === 2) {
+    console.log("Starting the game.");
+    socket.broadcast.emit("game-start");
+  }
 };
 
-const removeClient = socket => {
-    console.log("Client has disconnected.", socket.id);
-    delete clientsConnected[socket.id];
+const removeClient = (socket) => {
+  console.log("Client has disconnected.", socket.id);
+  delete clientsConnected[socket.id];
 };
 
-io.on("connection", socket => {
-    let id = socket.id;
+io.on("connection", (socket) => {
+  let id = socket.id;
 
-    addClient(socket);
+  addClient(socket);
 
-    socket.on("disconnect", () => {
-        removeClient(socket);
-        socket.emit("clientdisconnect", id);
-    })
-})
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("clientdisconnect", id);
+    removeClient(socket);
+  });
+});
 
-function isThereAnOpponent(socket) {
-    if (!players[socket.id].opponent) {
-        return;
-    }
-    return players[players[socket.id].opponent].socket;
+function convertSocketToPlayer(socket) {
+  firstPlayer
+    ? (secondPlayer = {
+        playersSocket: socket,
+        symbol: "O",
+        firstPlayer: false,
+        opponentsSocket: null,
+      })
+    : (firstPlayer = {
+        playersSocket: socket,
+        symbol: "X",
+        firstPlayer: true,
+        opponentsSocket: null,
+      });
 }
 
-
+function isThereAnOpponent(socket) {
+  if (!players[socket.id].opponent) {
+    return;
+  }
+  return players[players[socket.id].opponent].socket;
+}
 
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
