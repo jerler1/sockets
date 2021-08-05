8c6d3044f7ff5f17b7d617adaec7705fce10c22e
Bunks <busk_1@yahoo.com>
2021-08-05 16:00:21 -0400

    added client emitter that sends the server the move just made with the player data

--- client.js
+++ client.js
@@ -6,17 +6,46 @@ const port = process.argv[3];
 const socket = io(`http://${serverIp}:${port}`);
 
 socket.on("connect", () => {
-    console.log("Client has connected with id: " + socket.id);
-})
+  console.log(
+    "Connected to " + serverIp + " " + port + ". Socket id is: " + socket.id
+  );
+});
 
 socket.on("game-start", () => {
-    console.log("The game has started.");
-})
+  console.log("The game has started.");
+});
 
-socket.on("clientdisconnect", id => {
-    console.log("A client has disconnected.");
-    console.log(id);
-})
+socket.on("boardState", (boardState) => {
+  console.log(boardState);
+});
 
+socket.on("clientdisconnect", (id) => {
+  console.log("A client has disconnected.");
+  console.log(id);
+});
 
-console.log(process.argv);
+socket.on("yourTurn", (data) => {
+  console.log(data.message);
+  require("readcommand").read(
+    { history: ["Resign", "1", "2", "3", "4", "5", "6", "7", "8", "9"] },
+    function (err, args) {
+      socket.emit("moveMade", { socketId: socket.id, selection: args[0], player: data.player });
+    }
+  );
+});
+
+socket.on("opponentsTurn", (data) => {
+  console.log(data);
+  //   require("readcommand").read(
+  //     { history: ["Resign", "r"] },
+  //     function (err, args) {
+  //       console.log(args);
+  //       console.log("Arguments: %s", JSON.stringify(args));
+  //     }
+  //   );
+});
+
+// Tells the client the game has started and if they are first or second.
+socket.on("gameStartMessage", (message) => {
+  console.log(message);
+});
